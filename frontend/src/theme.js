e3d8dee650628713d628a3f200f6049e99f58ab4
natalino-coelho <natalino.coelho@gmail.com>
2024-10-14 21:10:05 -0300

    #

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -3,14 +3,14 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#10a5d2',
+      main: '#032996',
     },
     secondary: {
-      main: '#bcbcbc',
+      main: '#123456',
     },
     // Definindo uma paleta personalizada para a sidebar
     drawer: {
-      background: '#5d6a6e', // Cor de fundo do drawer (sidebar)
+      background: '#031a4a', // Cor de fundo do drawer (sidebar)
       color: '#ffffff',       // Cor do texto e ícones
     },
   },
